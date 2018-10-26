adaf44172377ad32a5ea3a5845a43831e2565320
udaysankarm <42138080+udaysankarm@users.noreply.github.com>
2018-10-26 01:58:41 -0400

    Update lab9-q10.cpp

--- lab9-q10.cpp
+++ lab9-q10.cpp
@@ -4,12 +4,12 @@ using namespace std;
 void revString(char* p)
 {
   int l,i=0;
-  char temp;
-  for(i=0;i<2;i++)
+  char temp;//we are swapping the elements tempmis used store the vharacters temporarly
+  for(i=0;i<2;i++)//we sholu swap avaery element except the middle element
   { 
-    temp=*(p+i);
-    *(p+i)=*(p-i+4);
-    *(p-i+4)=temp;
+    temp=*(p+i); //storing the i th element to temp
+    *(p+i)=*(p-i+4); //storing the ith character from last to the i th position
+    *(p-i+4)=temp;//storing ith to charcter to the ith position from last
   }
 }
 int main()
